0086e15e172a34a09864c35d6de7f74193e2d014
sachin modha <64076209+sach-71@users.noreply.github.com>
2020-09-25 11:14:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  alert("WELCOME TO SACHIN MODHA PAGE :)")
